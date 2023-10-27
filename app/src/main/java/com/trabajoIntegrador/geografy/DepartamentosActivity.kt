68dcb269eb8f067a9afaab60f4b1c7a4e76ea4e0
Guillermina Charles <chm.guillermina@gmail.com>
2023-10-27 11:50:06 -0300

    Proyecto base

---
+++ app/src/main/java/com/trabajoIntegrador/geografy/DepartamentosActivity.kt
@@ -0,0 +1,26 @@
+package com.example.geografiapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class DepartamentosActivity: AppCompatActivity() {
+
+    lateinit var tvDepto: TextView
+
+    //var departamentosSalta: ArrayList<String> = arrayListOf("San Martín", "Rivadavia", "Salta", "Oran", "La vinia", "Metan")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_departamentos)
+
+        tvDepto = findViewById(R.id.depto)
+
+        val datoRecibido = intent.getStringExtra("departamentos")
+        //var departamentosSalta: String = "Oran"
+        tvDepto.text = datoRecibido.toString()
+
+
+    }
+}
+
